cb01df978dc1ca97a42646302c4899240503fa15
Arthur <arthur.costa80@etec.sp.gov.br>
2024-11-04 21:31:48 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,29 @@
+function appendNumber(number) {
+    const display = document.getElementById('display');
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    const display = document.getElementById('display');
+    if (display.value && !isNaN(display.value.slice(-1))) { 
+        display.value += operator;
+    }
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Erro';
+    }
+}
